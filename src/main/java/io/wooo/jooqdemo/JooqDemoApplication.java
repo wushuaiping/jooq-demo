3ebb19d57ef71b632bc7aa73534fe2d3f7261ff2
Spring <spwu@creams.io>
2019-03-06 10:01:55 +0800

    init

---
+++ src/main/java/io/wooo/jooqdemo/JooqDemoApplication.java
@@ -0,0 +1,13 @@
+package io.wooo.jooqdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JooqDemoApplication.class, args);
+	}
+
+}
